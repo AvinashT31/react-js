688cdf5172d135d44c31ab57e4a40e0782a8c904
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-03-15 08:03:11 +0530

    update

--- awdiz/src/components/Register.jsx
+++ awdiz/src/components/Register.jsx
@@ -6,16 +6,26 @@ function Register() {
   console.log(userData, "userData");
 
    function clickHandle(e){
-    // alert("working");
+   
      e.preventDefault();
+    //   alert(userData);
+    var DataFromLS = JSON.parse(localStorage.getItem("userForReact")) || [];
+    DataFromLS.push(userData);
+    localStorage.setItem("userForReact", JSON.stringify(DataFromLS));
+    setuserData({name: "", email: "", password: ""});
+    alert("registration done");
+    
 
    }
 
    function handle(e){
 
     e.preventDefault();
-    console.log(e);
-    setuserData(e.target.name);
+    // console.log(e);
+    var name = e.target.name;
+    var value = e.target.value;
+    // console.log(e.target.name, e.target.value);
+    setuserData({...userData, [name]: value})
    }
    
 
@@ -26,15 +36,15 @@ function Register() {
             <form onSubmit={(e) => clickHandle(e)}>
                 <label>Enter your Name</label>
                 <br />
-                <input onChange={(e) => handle(e)} name="userName" value={name} type="text" placeholder="Enter your Name" />
+                <input onChange={(e) => handle(e)} name="name" type="text" placeholder="Enter your Name" />
                 <br />
                 <label>Enter your Email</label>
                 <br />
-                <input onChange={(e) => handle(e)} name="userEmail" value={email} type="email" placeholder="Enter your Email" />
+                <input onChange={(e) => handle(e)} name="email" type="email" placeholder="Enter your Email" />
                 <br />
                 <label>Enter your Password</label>
                 <br />
-                <input onChange={(e) =>handle(e)} name="userPassword" value={password} type="password" placeholder="Enter your Password" />
+                <input onChange={(e) =>handle(e)} name="password" type="password" placeholder="Enter your Password" />
                 <br />
                 <input type="submit" />
             </form>
